8b27602262a8cb5ab292743651bfbdea7ff6164f
xpwu <diodexp@foxmail.com>
2023-02-23 17:46:43 +0800

    update go.mod

--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.16
 require (
 	github.com/xpwu/go-cmd v0.1.0
 	github.com/xpwu/go-log v0.1.0
-	github.com/xpwu/go-stream v0.1.0
+	github.com/xpwu/go-streamclient v0.0.0-20230223094428-304942ba0bb0
 )
--- main/main.go
+++ main/main.go
@@ -1,44 +1,44 @@
 package main
 
 import (
-  "context"
-  "github.com/xpwu/go-cmd/arg"
-  "github.com/xpwu/go-cmd/cmd"
-  "github.com/xpwu/go-log/log"
-  "github.com/xpwu/go-stream/push/pushc"
-  "time"
+	"context"
+	"github.com/xpwu/go-cmd/arg"
+	"github.com/xpwu/go-cmd/cmd"
+	"github.com/xpwu/go-log/log"
+	"github.com/xpwu/go-streamclient/pushc"
+	"time"
 )
 
 func main() {
-  cmd.RegisterCmd("push", "push data to client", func(args *arg.Arg) {
-    pushUrl := ""
-    args.String(&pushUrl, "u", "push url")
-    data := ""
-    args.String(&data, "d", "data")
+	cmd.RegisterCmd("push", "push data to client", func(args *arg.Arg) {
+		pushUrl := ""
+		args.String(&pushUrl, "u", "push url")
+		data := ""
+		args.String(&data, "d", "data")
 
-    args.Parse()
+		args.Parse()
 
-    err := pushc.PushData(context.TODO(), pushUrl, []byte(data), 10*time.Second)
-    if err != nil {
-      log.Error(err)
-    } else {
-      log.Info("push ok")
-    }
-  })
+		err := pushc.PushData(context.TODO(), pushUrl, []byte(data), 10*time.Second)
+		if err != nil {
+			log.Error(err)
+		} else {
+			log.Info("push ok")
+		}
+	})
 
-  cmd.RegisterCmd("close", "close client", func(args *arg.Arg) {
-    pushUrl := ""
-    args.String(&pushUrl, "u", "push url")
+	cmd.RegisterCmd("close", "close client", func(args *arg.Arg) {
+		pushUrl := ""
+		args.String(&pushUrl, "u", "push url")
 
-    args.Parse()
+		args.Parse()
 
-    err := pushc.Close(context.TODO(), pushUrl)
-    if err != nil {
-      log.Error(err)
-    } else {
-      log.Info("close ok")
-    }
-  })
+		err := pushc.Close(context.TODO(), pushUrl)
+		if err != nil {
+			log.Error(err)
+		} else {
+			log.Info("close ok")
+		}
+	})
 
-  cmd.Run()
+	cmd.Run()
 }
